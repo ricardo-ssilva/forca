8779c517a539f9e72af6dc5c939f2683d4b21844
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-05-23 16:50:17 -0300

    Finaliza Partes da primeira pag

--- main.js
+++ main.js
@@ -9,7 +9,40 @@ function drawRows(){
     }
 }
 
-function date () {
-    
+function writeDate () {
+    let data = new Date ()
+    let dia = String(data.getDate()).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
+    let ano = data.getFullYear()
+    let diaSemana = data.getDay()
+
+    switch(diaSemana){
+        case 0: 
+        document.querySelector('#dom').checked = true;
+        break;
+        case 1:
+            document.querySelector('#seg').checked = true;
+            break;
+        case 2:
+            document.querySelector('#ter').checked = true;
+            break;
+        case 3:
+            document.querySelector('#qua').checked = true;
+            break;
+        case 4:
+            document.querySelector('#qui').checked = true;
+            break;
+        case 5:
+            document.querySelector('#sex').checked = true;
+            break;
+        case 6:
+            document.querySelector('#sab').checked = true;
+            break;
+    }
+
+    document.querySelector('.dia').innerHTML = dia
+    document.querySelector('.mes').innerHTML = mes
+    document.querySelector('.ano').innerHTML = ano
 }
+writeDate()
 drawRows()
