ae85d9d86ba04a0f2842e79915be1b588185cd5a
Ricardo Silva <ricardo.ssilva197@gmail.com>
2022-05-23 20:16:00 -0300

    finaliza primeira parte

--- main.js
+++ main.js
@@ -8,41 +8,46 @@ function drawRows(){
     
     }
 }
-
 function writeDate () {
-    let data = new Date ()
-    let dia = String(data.getDate()).padStart(2, '0');
-    let mes = String(data.getMonth() + 1).padStart(2, '0');
-    let ano = data.getFullYear()
-    let diaSemana = data.getDay()
 
-    switch(diaSemana){
-        case 0: 
-        document.querySelector('#dom').checked = true;
+    //day - month - year
+    let date = new Date ()
+    let day = String(date.getDate()).padStart(2, '0');
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let year = date.getFullYear()
+  
+
+    document.querySelector('.dia').innerHTML = day
+    document.querySelector('.mes').innerHTML = month
+    document.querySelector('.ano').innerHTML = year
+
+    //day of week
+    let dayweek = date.getDay()
+    switch(dayweek){
+        case 1: 
+        document.querySelector('.mon').style.backgroundColor = "black" ;
         break;
-        case 1:
-            document.querySelector('#seg').checked = true;
-            break;
         case 2:
-            document.querySelector('#ter').checked = true;
+            document.querySelector('.tue').style.backgroundColor = "black"
             break;
         case 3:
-            document.querySelector('#qua').checked = true;
+            document.querySelector('.wed').style.backgroundColor = "black"
             break;
         case 4:
-            document.querySelector('#qui').checked = true;
+            document.querySelector('.thu').style.backgroundColor = "black"
             break;
         case 5:
-            document.querySelector('#sex').checked = true;
+            document.querySelector('.fry').style.backgroundColor = "black"
             break;
         case 6:
-            document.querySelector('#sab').checked = true;
+            document.querySelector('.sat').style.backgroundColor = "black"
+            break;
+        case 0:
+            document.querySelector('.sun').style.backgroundColor = "black"
             break;
     }
 
-    document.querySelector('.dia').innerHTML = dia
-    document.querySelector('.mes').innerHTML = mes
-    document.querySelector('.ano').innerHTML = ano
+ 
 }
 writeDate()
-drawRows()
+setInterval(drawRows, 500)
